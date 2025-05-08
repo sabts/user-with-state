f915eeb7a9ad39333a8dc2750203335fe3cccfed
sabts <34947635+sabts@users.noreply.github.com>
2025-05-08 17:06:16 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Button from "./components/button/Button";
+
+const App = () => {
+  return (
+    <>
+      <h1>Template React</h1>
+      <Button />
+    </>
+  );
+};
+
+export default App;
