7ffca8d5bf7d01d58606105418d68110f73715b3
sabts <sabrinataraschi@gmail.com>
2025-05-09 01:03:31 +0200

    .

--- src/components/user-card/usercard.jsx
+++ src/components/user-card/usercard.jsx
@@ -3,6 +3,7 @@ import styles from "./usercard.module.css"
 
 const UserCard = ({profileImage, name, username, active}) => {
     const getStatusText = active ? "Activo" : "Inactivo";
+    const statusClass =`${styles.status} ${active ? styles.active : styles.inactive}`;
 return <div className={styles["user-card"]}>
     <div className={styles["userinfo"]}>
     <img src={profileImage} className={styles["photo-container"]}/>
@@ -11,7 +12,7 @@ return <div className={styles["user-card"]}>
         <p className={styles["username"]}>@{username}</p>
     </div>
     </div>
-    <span>{getStatusText}</span>
+    <span className={statusClass}>{getStatusText}</span>
     <Button>Ver Detalles</Button>
 </div>
 }
